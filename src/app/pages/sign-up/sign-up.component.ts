f43ff9688374b4476ed4a5f3a1ae177f2687ac8a
Victoire Youngui <victoire.ntchuigwa@2026.ucac-icam.com>
2024-09-03 11:57:10 +0100

    singup page

--- src/app/pages/sign-up/sign-up.component.ts
+++ src/app/pages/sign-up/sign-up.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {LogoComponent} from "../../components/logo/logo.component";
+import {LucideAngularModule} from "lucide-angular";
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [],
+  imports: [
+    LogoComponent,
+    LucideAngularModule,
+  ],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.css'
 })
