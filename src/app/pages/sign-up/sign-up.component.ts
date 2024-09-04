8945dc454e5164c2b71fa0e75bf065f5d89effd7
Victoire Youngui <victoire.ntchuigwa@2026.ucac-icam.com>
2024-09-04 03:28:20 +0100

    input fields + submitted button.

--- src/app/pages/sign-up/sign-up.component.ts
+++ src/app/pages/sign-up/sign-up.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import {LogoComponent} from "../../components/logo/logo.component";
 import {LucideAngularModule} from "lucide-angular";
+import {InputFieldComponent} from "../../components/input-field/input-field.component";
+import {ButtonComponent} from "../../components/button/button.component";
 
 @Component({
   selector: 'app-sign-up',
@@ -8,11 +10,38 @@ import {LucideAngularModule} from "lucide-angular";
   imports: [
     LogoComponent,
     LucideAngularModule,
+    InputFieldComponent,
+    ButtonComponent,
   ],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.css'
 })
 export class SignUpComponent {
+
+  inputFieldsProperties = [
+    {
+      label: "Name",
+      type: "text",
+      placeholder: "Name",
+    },
+
+    {
+      label: "Email",
+      type: "email",
+      placeholder: "Email",
+    },
+  ]
+
+  buttonProperties = [
+    {
+      name : "Submit",
+      backgroundColor : "#000"
+    },
+    {
+      name : "Cancel",
+      backgroundColor : "#fff"
+    },
+  ]
 
 
 }
