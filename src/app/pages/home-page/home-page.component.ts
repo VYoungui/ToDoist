2b4b883a2efbb53f113ce651f2798b219f078fc8
Victoire Youngui <victoire.ntchuigwa@2026.ucac-icam.com>
2024-09-05 08:52:59 +0100

    navbar completed

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,15 +1,22 @@
 import { Component } from '@angular/core';
 import {HeaderLayoutComponent} from "../../components/header-layout/header-layout.component";
+import {ButtonComponent} from "../../components/button/button.component";
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
   imports: [
-    HeaderLayoutComponent
+    HeaderLayoutComponent,
+    ButtonComponent
   ],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
 export class HomePageComponent {
 
+  dateFilterOption = [
+    "Today",
+    "Yesterday",
+    "Tomorrow",
+  ]
 }
